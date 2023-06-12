dbca6d26ddde5eee8a29bc03b786f6ad4801088f
tamires <tamireshc@gmail.com>
2023-06-12 16:04:39 -0300

    feat: add exercise hashSet

---
+++ hashSet/Entities/Course.cs
@@ -0,0 +1,7 @@
+namespace HashSet.Entities;
+
+public class Course
+{
+    public string Name { get; set; }
+    public List<int> Students { get; set; }
+}
---
+++ hashSet/Program.cs
@@ -0,0 +1,22 @@
+﻿using HashSet.Entities;
+
+Course A = new Course { Name = "A" };
+Course B = new Course { Name = "B" };
+Course C = new Course { Name = "C" };
+
+List<Course> ListCourses = new List<Course>() { A, B, C };
+HashSet<int> Registrations = new HashSet<int>();
+
+foreach (Course course in ListCourses)
+{
+    Console.WriteLine($"How many students for course {course.Name}?");
+    int n = int.Parse(Console.ReadLine());
+    Console.WriteLine("Type the registrations");
+    for (int i = 0; i < n; i++)
+    {
+        int numberOfRegistration = int.Parse(Console.ReadLine());
+        Registrations.Add(numberOfRegistration);
+    }
+}
+
+Console.WriteLine($"Total students: {Registrations.Count}");
