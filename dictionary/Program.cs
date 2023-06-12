91a3739e7d7724c60187d909d5513b05fe12cccd
tamires <tamireshc@gmail.com>
2023-06-12 16:39:05 -0300

    feat: add exercise dictonary

---
+++ dictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Total Voutes");
+string path = "/Users/tamireshc/Desktop/CursoCSharp/dictionary/in.txt";
+
+Dictionary<string, int> candidates = new Dictionary<string, int>();
+StreamReader sr = null;
+try
+{
+    sr = File.OpenText(path);
+    while (!sr.EndOfStream)
+    {
+        string[] line = sr.ReadLine().Split(",");
+        string name = line[0];
+        int voute = int.Parse(line[1]);
+        if (!candidates.ContainsKey(name))
+        {
+            candidates[name] = voute;
+        }
+        else
+        {
+            candidates[name] += voute;
+        }
+    }
+    foreach (var item in candidates)
+    {
+        Console.WriteLine($"{item.Key} : {item.Value}");
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error occurred");
+    Console.WriteLine(e.Message);
+}
+finally
+{
+    if (sr != null) sr.Close();
+}
+
+
