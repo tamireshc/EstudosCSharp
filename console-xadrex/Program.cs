3d4a84ec0b737ffd1390a0ecd0a895bd777669c8
tamireshc <tamireshc@gmail.com>
2023-06-14 08:18:22 -0300

    feat:criando a classe posicao xadrez

--- console-xadrex/Program.cs
+++ console-xadrex/Program.cs
@@ -4,11 +4,12 @@ using xadrez_console;
 
 
 Tabuleiro tab = new Tabuleiro(8, 8);
+PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+Console.WriteLine(pos);
 
 try
 {
   tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-  tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
   tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
   Tela.imprimitTabuleiro(tab);
 }
